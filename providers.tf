c3bb4dcf9e8c4f0a5fb27dad9a16806bd5dda3f8
Dev Mukherjee <devraj@gmail.com>
2022-06-05 11:41:53 +0930

    refactor: file structure

---
+++ providers.tf
@@ -0,0 +1,10 @@
+
+# The Linode provider requires an API token that you can
+# generate using the linode-cli (check README)
+# https://registry.terraform.io/providers/linode/linode/latest/docs
+#
+# This is ultimately stored in Terraform cloud
+provider "linode" {
+  token = var.linode_token
+}
+

678c7d0d5fe8473b77cd5a12328c7732a2dffd66
Dev Mukherjee <devraj@gmail.com>
2022-06-05 14:40:54 +0930

    feat: configure helm provider

--- providers.tf
+++ providers.tf
@@ -8,3 +8,31 @@ provider "linode" {
     token = var.linode_token
 }
 
+# For the following providers we load the values from the secrets
+# that the Linode Kubernetes resource will provide for us.
+#
+# The kubectl configuration which is available via the output
+# terraform output kubeconfig 
+# is a base64 encoded yaml file, which will be decoded and parsed
+provider "helm" {
+    kubernetes {
+        host = "${yamldecode(civo_kubernetes_cluster.k8s-cluster.kubeconfig).clusters.0.cluster.server}"
+        client_certificate = "${base64decode(yamldecode(civo_kubernetes_cluster.k8s-cluster.kubeconfig).users.0.user.client-certificate-data)}"
+        client_key = "${base64decode(yamldecode(civo_kubernetes_cluster.k8s-cluster.kubeconfig).users.0.user.client-key-data)}"
+        cluster_ca_certificate ="${base64decode(yamldecode(civo_kubernetes_cluster.k8s-cluster.kubeconfig).clusters.0.cluster.certificate-authority-data)}"
+    }
+}
+provider "kubernetes" {
+    host = "${yamldecode(civo_kubernetes_cluster.k8s-cluster.kubeconfig).clusters.0.cluster.server}"
+    client_certificate = "${base64decode(yamldecode(civo_kubernetes_cluster.k8s-cluster.kubeconfig).users.0.user.client-certificate-data)}"
+    client_key = "${base64decode(yamldecode(civo_kubernetes_cluster.k8s-cluster.kubeconfig).users.0.user.client-key-data)}"
+    cluster_ca_certificate = "${base64decode(yamldecode(civo_kubernetes_cluster.k8s-cluster.kubeconfig).clusters.0.cluster.certificate-authority-data)}"
+}
+
+provider "kubectl" {
+    host = "${yamldecode(civo_kubernetes_cluster.k8s-cluster.kubeconfig).clusters.0.cluster.server}"
+    client_certificate = "${base64decode(yamldecode(civo_kubernetes_cluster.k8s-cluster.kubeconfig).users.0.user.client-certificate-data)}"
+    client_key = "${base64decode(yamldecode(civo_kubernetes_cluster.k8s-cluster.kubeconfig).users.0.user.client-key-data)}"
+    cluster_ca_certificate = "${base64decode(yamldecode(civo_kubernetes_cluster.k8s-cluster.kubeconfig).clusters.0.cluster.certificate-authority-data)}"
+    load_config_file = false
+}
